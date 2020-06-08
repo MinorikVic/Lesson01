db1f14bc85c114b097b646efb2a8ceb5b1a26eec
MinorikVic <pobeda1105.94@gmail.com>
2020-06-08 23:50:16 +0300

    Lesson03

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period,
-    budgetDay;
+'use strict';
 
-money = 35800;
-income = 'Подработка 15000';
-addExpenses = 'Комунальные платежи, Продукты, Транспорт';
-deposit = true;
-mission = 2000000;
-period = 5;
+
+let money = 35800,
+income = 'Подработка 15000',
+addExpenses = 'Комунальные платежи, Продукты, Транспорт',
+deposit = true,
+mission = 2000000,
+period = 5,
 budgetDay = money / 30;
 
-console.log(money);
-console.log(income);
-console.log(deposit);
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' +mission + '$');
-console.log(addExpenses.toLocaleLowerCase());
-console.log(addExpenses.split(', '));
-console.log(budgetDay);
+console.log(typeof prompt('Ваш месячный доход?', '35800'));
+console.log(typeof prompt('Перечислите возможные расходы за рассчитываемый период через запятую'));
+console.log(typeof prompt('Есть ли у вас депозит в банке?'));
