dd7a1a33fb4854281a381bcee59725810df82406
MinorikVic <pobeda1105.94@gmail.com>
2020-06-12 21:51:05 +0300

    Lesson_04

--- script.js
+++ script.js
@@ -17,46 +17,43 @@ addExpenses = addExpenses.split(',');
 accumulatedMonth = getAccumulatedMonth();
 budgetDay = accumulatedMonth / 30;
 
-if (budgetDay >= 1200) {
-    alert('У вас высокий уровень дохода');
-}
-if ((budgetDay >= 600 ) && (budgetDay < 1200)) {
-    alert('У вас средний уровень дохода');
-}
-if ((budgetDay < 600 ) && (budgetDay > 0)) {
-    alert('К сожалению, у вас низкий уровень дохода');
-}
-if (budgetDay <= 0 ) {
-    alert('Что то пошло не так');
-}
 
+let showTypeOf = function(data) {
+    console.log(data, typeof(data));
+};
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+let getStatusIncome = function() {
+    if (budgetDay >= 1200) {
+        alert('У вас высокий уровень дохода');
+    } else if ((budgetDay >= 600 ) && (budgetDay < 1200)) {
+        alert('У вас средний уровень дохода');
+    } else if ((budgetDay < 600 ) && (budgetDay > 0)) {
+        alert('К сожалению, у вас низкий уровень дохода');
+    } else if (budgetDay <= 0 ) {
+        alert('Что то пошло не так');
+    }
+};
+
+getStatusIncome();
 
 function getExpensesMonth() {
     return (amount1 + amount2);
 }
-console.log(getExpensesMonth());
-
-
 
 function getAccumulatedMonth() {
-    return Math.ceil(mission / getExpensesMonth());
+    return money - getExpensesMonth();
 }
-console.log(getAccumulatedMonth());
-
-
 
 function getTargetMonth() {
-    return (accumulatedMonth);
+    return Math.ceil(mission / accumulatedMonth);
 }
-console.log(getTargetMonth());
 
-
-console.log('Бюджет на месяц: '+ money);
 console.log('Статьи расходов: '+ addExpenses);
-console.log('Депозит есть: '+ deposit);
-console.log('Статья расходов первая: '+ expenses1);
-console.log('Расход по первой статье: '+ amount1);
-console.log('Статья расходов вторая: '+ expenses2);
-console.log('Расход по второй статье: '+ amount2);
-console.log('Цель заработать: '+ mission);
 console.log('Бюджет на день: '+ budgetDay);
+console.log(getExpensesMonth());
+console.log(getAccumulatedMonth());
+console.log(getTargetMonth());
